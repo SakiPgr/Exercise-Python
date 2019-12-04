dff1fddee306772d856ea5e6da14f31b2e2d416e
SakiPgr <realsaki@hotmail.gr>
2019-12-04 07:05:38 +0200

    Added the next to exercises with solutions and info about used librarys. Added to the readme the version of python I use.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Python exercises from https://www.w3resource.com/python-exercises/
 
 I intend to solve as much as I can from the page. If I see that something is a problem than I'll read about what
 I need so I can solve this and be prepaired for similar problems.
+
+
+I use Python 3.7.4 for those exercises
--- exercises001.py
+++ exercises001.py
@@ -49,4 +49,63 @@ print('\n\n')
 # written in python 2 there shouldn't be any major difference in using the sys module.
 
 
-# 3. 
+# 3. Write a Python program to display the current date and time.
+
+# Sample Output
+# Current date and time
+# 2014-07-05 14:34:14
+
+# My solution
+import datetime
+
+time_now = datetime.datetime.now()
+print(time_now.strftime('%y-%m-%d %H:%M:%S'))
+print()
+
+# Output of my solution is: 19-12-04 06:37:55
+# the datetime module is probably one that is going to be used a lot. I have used it 
+# in some occasions but that does not mean I know it well. My solution is slightly different
+# from what was asked. 
+
+# Solution as given on the site
+# I won't import datetime again
+# import datetime
+now = datetime.datetime.now()
+print('Current date and time : ')
+print(now.strftime('%Y-%m-%d %H:%M:%S'))
+print("\n")
+
+# To find more information about the datetime module have a look at the official site
+# https://docs.python.org/3.7/library/datetime.html or maybe at this one
+# https://www.w3schools.com/python/python_datetime.asp or this which has even more examples
+# https://www.programiz.com/python-programming/datetime
+
+
+# 4. Write a Python program which accepts the radius of a circle from the user and compute
+# the area.
+
+# Now to get the area of a circle you can simply search for that on the internet should you
+# not remember how to compute that. No biggy.
+# Or use this radius (r) to the power of 2 (**2) times pi (π)
+# Or short r**2 * π
+
+# My solution
+import math  # I import math to get the value for pi (π)
+
+radius = float(input("Please enter the radius: "))
+print(radius**2 * math.pi)
+
+# The output is 3.8013271108436504 which is the same as given by the site
+# The math module is very important if you have to make computations with
+# higher precision and you can import constants to use for your programs
+
+# Solution as given by the site
+from math import pi
+r = float(input("Input the radius of the circle : "))
+print("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+print("\n\n")
+
+# To find more information about the math module look up the official site
+# https://docs.python.org/3.7/library/math.html or this one if this helps
+# https://www.programiz.com/python-programming/modules/math
+
